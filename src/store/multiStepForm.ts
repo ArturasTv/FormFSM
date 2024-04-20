17e3ff5ed933ea9ba099e03df662ed936517e0de
Arturas Tvaronas <arturastvaronas@inbox.lt>
2024-04-20 07:17:36 -0400

    create multi step form zustand store

---
+++ src/store/multiStepForm.ts
@@ -0,0 +1,42 @@
+import { create } from 'zustand'
+
+type MultiStepFormState = "first" | "second" | "third";
+
+type Transition = {
+    from: MultiStepFormState;
+    to: MultiStepFormState;
+    trigger: string;
+}
+
+type MultiStepFormStore = {
+  state: MultiStepFormState;
+  next: () => void; 
+  previous: () => void;
+}
+
+const transitions: Transition[] = [
+    { from: "first", to: "second", trigger: "next" },
+    { from: "second", to: "third", trigger: "next" },
+    { from: "third", to: "first", trigger: "next" },
+    { from: "second", to: "first", trigger: "previous" },
+    { from: "third", to: "second", trigger: "previous" },
+    { from: "first", to: "third", trigger: "previous" }
+    ];
+
+const useBearStore = create<MultiStepFormStore>()((set, get) => ({
+  state: "first",
+    next: () => {
+        const current = transitions.find((transition) => transition.from === get().state && transition.trigger === "next")
+        if (current) {
+        set({ state: current.to })
+        }
+    },
+    previous: () => {
+        const current = transitions.find((transition) => transition.from === get().state && transition.trigger === "previous")
+        if (current) {
+        set({ state: current.to })
+        }
+    }
+}))
+
+export default useBearStore;
